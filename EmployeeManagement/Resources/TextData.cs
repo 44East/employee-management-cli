236a3e5a9fddbfa8259ce9f5328dc8581f465bf6
44East <antswert@gmail.com>
2023-08-26 14:24:45 +0300

    Edit code, refact mistakes in main menu method calls

--- EmployeeManagement/Resources/TextData.cs
+++ EmployeeManagement/Resources/TextData.cs
@@ -14,16 +14,18 @@
         public static string InsertEmployeeSurname { get; } = "Введите фамилию сотрудника:";
         public static string InsertEmployeeAge { get; } = "Введите возраст сотрудника:";
         public static string InsertAgeFailure { get; } = "Некорректный возраст. Возраст должен быть числом.";
-        public static string InsertEmailEmployee { get;  } = "Введите email сотрудника:";
+        public static string InsertEmailEmployee { get; } = "Введите email сотрудника:";
         public static string InsertEmployeePhone { get; } = "Введите номер телефона сотрудника:";
         public static string EmployeeAddedSuccess { get; } = "Сотрудник успешно добавлен.";
         public static string EmployeeSearchBar { get; } = "=================================================\n" +
                                                           "|Поиск сотрудника:                              |\n" +
                                                           "=================================================\n";
         public static string EmployeeFoundSuccess { get; } = "Список найденных сотрудников:";
-        public static string EmployeeAllDataString { get; } = "Имя: {0}, Фамилия: {1}, Возраст: {2}, Email: {3}, Телефон: {4}";               
+        public static string EmployeeAllDataString { get; } = "Имя: {0}, Фамилия: {1}, Возраст: {2}, Email: {3}, Телефон: {4}";
         public static string EmployeeSearchFailure { get; } = "Вы не ввели по крайней мере ожно из значений!";
         public static string EmployeeSearchNotFound { get; } = "Не найдено совпадений!";
         public static string GetReadyButton { get; } = "Для продолжения нажмите любую клавишу";
+        public static string EntityListEmpty { get; } = "Упс, список пуст, возможно вы ничего не добавили в файл!";
+        public static string MainMenuInputFail { get; } = "Ошибка, попробуйте еще раз!";
     }
 }
--- EmployeeManagement/Services/MainMenu.cs
+++ EmployeeManagement/Services/MainMenu.cs
@@ -15,32 +15,29 @@ namespace EmployeeManagement.Services
             _dataWorker = new DataWorker<Employee>();
         }
         public void Run()
-        {
-            Console.Clear();
+        { 
             bool runner = true;
             while (runner)
             {
+                Console.Clear();
                 Console.WriteLine(TextData.MainText);
                 var userSelect = Console.ReadLine()?.Trim();
                 switch (userSelect)
                 {
                     case "1":
-                        runner = false;
                         InsertEmployee();
                         break;
                     case "2":
-                        runner = false;
                         FindTheEmployee();
                         break;
                     case "3":
-                        runner = false;
                         GetTheAllEmployeeList();
                         break;
                     case "q":
-                        runner = false;
                         break;
                     default:
-                        Console.WriteLine("Try again!");
+                        Console.WriteLine(TextData.MainMenuInputFail);
+                        Task.Delay(1000).Wait();
                         break;
 
                 }
@@ -82,11 +79,13 @@ namespace EmployeeManagement.Services
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                Task.Delay(1000).Wait();
+                return;
             }
-            Run();
+            return;
         }
         private void FindTheEmployee()
-        {            
+        {
             bool runner = true;
             while (runner)
             {
@@ -100,35 +99,63 @@ namespace EmployeeManagement.Services
 
                 if (!string.IsNullOrEmpty(employeeName) && !string.IsNullOrEmpty(employeeSurname))
                 {
-                    var foundEmployeeCollection = _finder.GetEmployeeByName(employeeName, employeeSurname, _dataWorker.GetTheEntityList());
-                    if (foundEmployeeCollection.Count() > 0)
+                    var foundEmployeeCollection = new List<Employee>();
+                    try
                     {
-                        Console.WriteLine(TextData.EmployeeFoundSuccess);
-                        foreach (var employee in foundEmployeeCollection)
+                        foundEmployeeCollection = _finder.GetEmployeeByName(employeeName, employeeSurname, _dataWorker.GetTheEntityList());
+                        if (foundEmployeeCollection.Count() > 0)
                         {
-                            Console.WriteLine(string.Format(TextData.EmployeeAllDataString,
-                                                            employee.Name,
-                                                            employee.Surname,
-                                                            employee.Age,
-                                                            employee.Email,
-                                                            employee.PhoneNumber));
+                            Console.WriteLine(TextData.EmployeeFoundSuccess);
+                            foreach (var employee in foundEmployeeCollection)
+                            {
+                                Console.WriteLine(string.Format(TextData.EmployeeAllDataString,
+                                                                employee.Name,
+                                                                employee.Surname,
+                                                                employee.Age,
+                                                                employee.Email,
+                                                                employee.PhoneNumber));
+                            }
+                            Console.WriteLine(TextData.GetReadyButton);
+                            Console.ReadKey();
+                            return;
                         }
-                        Console.WriteLine(TextData.GetReadyButton);
-                        Console.ReadKey();
-                        Run();
                     }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Task.Delay(2000).Wait();
+                        return;
+                    }
+
                     break;
                 }
                 Console.WriteLine(TextData.EmployeeSearchFailure);
             }
             Console.WriteLine(TextData.EmployeeSearchNotFound);
             Task.Delay(1000).Wait();
-            Run();
+            return;
         }
         private void GetTheAllEmployeeList()
         {
             Console.WriteLine(TextData.EmployeeFoundSuccess);
-            foreach (var employee in _dataWorker.GetTheEntityList())
+            var employeesCollection = new List<Employee>(); 
+            try
+            {
+                employeesCollection = _dataWorker.GetTheEntityList();
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                Task.Delay(1000).Wait();
+                return;
+            }
+            if (employeesCollection.Count == 0) 
+            {
+                Console.WriteLine(TextData.EntityListEmpty);
+                Task.Delay(1000).Wait();
+                return; 
+            }
+            foreach (var employee in employeesCollection)
             {
                 Console.WriteLine(string.Format(TextData.EmployeeAllDataString,
                                                 employee.Name,
@@ -137,9 +164,10 @@ namespace EmployeeManagement.Services
                                                 employee.Email,
                                                 employee.PhoneNumber));
             }
+
             Console.WriteLine(TextData.GetReadyButton);
             Console.ReadKey();
-            Run();
+            return;
         }
 
     }
